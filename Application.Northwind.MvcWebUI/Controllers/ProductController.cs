030f4f9c7193c02cc1c2fcaf09692728280d81f7
Safa Yilmaz <msafayilmaz@outlook.com>
2019-02-03 17:30:27 +0300

    First View Added

---
+++ Application.Northwind.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Northwind.MvcWebUI.Controllers
+{
+    public class ProductController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
