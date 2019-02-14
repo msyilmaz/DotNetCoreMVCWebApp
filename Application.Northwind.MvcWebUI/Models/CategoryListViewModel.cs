bcce95b94a70b03bb1b8dd002f3dbd842fb8b891
Safa Yilmaz <msafayilmaz@outlook.com>
2019-02-14 14:31:07 +0300

    Kategori butonu "active" özelliği

--- Application.Northwind.MvcWebUI/Models/CategoryListViewModel.cs
+++ Application.Northwind.MvcWebUI/Models/CategoryListViewModel.cs
@@ -9,5 +9,6 @@ namespace Application.Northwind.MvcWebUI.Models
     public class CategoryListViewModel
     {
         public List<Category> Categories { get; set; }
+        public int CurrentCategory { get; set; }
     }
 }
